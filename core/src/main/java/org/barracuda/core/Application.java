2d604aa0d4f17c0b8ee27964e6f64b57d0f29c60
Hannes Lowette <hanneslowette@live.be>
2016-01-15 14:02:30 +0100

    test

--- core/src/main/java/org/barracuda/core/Application.java
+++ core/src/main/java/org/barracuda/core/Application.java
@@ -1,16 +1,9 @@
 package org.barracuda.core;
 
-import org.barracuda.cdi.Horvik;
-import org.barracuda.cdi.HorvikContainer;
-
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
-		HorvikContainer container = new HorvikContainer();
-		Horvik horvik = new Horvik();
-		
-		System.out.println(horvik);
-		System.out.println(container);
+		System.out.println("starting service");
 	}
 
 }
