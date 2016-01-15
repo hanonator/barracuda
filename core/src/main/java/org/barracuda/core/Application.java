86eaa0eaa6b5c34a3b872d21bbd5b4c76c50d29a
Hannes Lowette <hanneslowette@live.be>
2016-01-15 13:39:01 +0100

    horvik

--- core/src/main/java/org/barracuda/core/Application.java
+++ core/src/main/java/org/barracuda/core/Application.java
@@ -1,9 +1,16 @@
 package org.barracuda.core;
 
+import org.barracuda.cdi.Horvik;
+import org.barracuda.cdi.HorvikContainer;
+
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
-		Thread.sleep(60_000L);
+		HorvikContainer container = new HorvikContainer();
+		Horvik horvik = new Horvik();
+		
+		System.out.println(horvik);
+		System.out.println(container);
 	}
 
 }
