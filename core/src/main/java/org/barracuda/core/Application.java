6a44cb56828ae4954fd73c4e4561c93a586902c3
Hannes Lowette <hanneslowette@live.be>
2016-01-15 12:57:45 +0100

    sleep for 60 seconds

--- core/src/main/java/org/barracuda/core/Application.java
+++ core/src/main/java/org/barracuda/core/Application.java
@@ -2,8 +2,8 @@ package org.barracuda.core;
 
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("ahoi sailor");
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(60_000L);
 	}
 
 }
