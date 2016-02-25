c015d5d6bdaabe23e0b2f94d4b052879edb3e860
Hannes Lowette <hanneslowette@live.be>
2016-02-25 14:08:41 +0100

    The walking controller is now able to be used in the application scope

--- core/src/main/java/org/barracuda/core/game/controller/WalkingController.java
+++ core/src/main/java/org/barracuda/core/game/controller/WalkingController.java
@@ -2,27 +2,20 @@ package org.barracuda.core.game.controller;
 
 import org.barracuda.core.game.event.MovementCommand;
 import org.barracuda.horvik.bean.Discoverable;
-import org.barracuda.horvik.context.session.SessionScoped;
+import org.barracuda.horvik.context.application.ApplicationScoped;
 import org.barracuda.horvik.event.Observes;
-import org.barracuda.horvik.inject.Inject;
 import org.barracuda.model.actor.Player;
 
-@SessionScoped
+@ApplicationScoped
 @Discoverable
 public class WalkingController {
-
-	/**
-	 * The player this controller is controlling (lol)
-	 */
-	@Inject
-	private Player player;
 
 	/**
 	 * Adds all of the queried waypoints to the player's movement queue
 	 * 
 	 * @param command
 	 */
-	public void on_walk(@Observes MovementCommand command) {
+	public void on_walk(@Observes MovementCommand command, Player player) {
 		player.getWaypoints().clear();
 		command.getWaypoints().forEach(player.getWaypoints()::add);
 		player.getWaypoints().finish();
