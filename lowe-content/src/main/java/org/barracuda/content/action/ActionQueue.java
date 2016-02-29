8ce51bd0d07c0d3dabfce2a4373f4eb6eba87ced
Hannes Lowette <hanneslowette@live.be>
2016-02-29 13:27:53 +0100

    Start to gathering skills

--- lowe-content/src/main/java/org/barracuda/content/action/ActionQueue.java
+++ lowe-content/src/main/java/org/barracuda/content/action/ActionQueue.java
@@ -9,7 +9,9 @@ public interface ActionQueue {
 
 	/**
 	 * The maximum amount of repetition is 120 as the idle logout timer is set to
-	 * 1 minute and the minimum delay is set to 1.
+	 * 1 minute and the minimum delay is set to 1. The only downside to this is that
+	 * the player will stop doing an action after 120 rotations, but he may or may
+	 * not actually be finished.
 	 */
 	int MAXIMUM_REPETITION = 120;
 
--- lowe-content/src/main/java/org/barracuda/content/skill/gather/GathererSkill.java
+++ lowe-content/src/main/java/org/barracuda/content/skill/gather/GathererSkill.java
@@ -1,13 +1,19 @@
 package org.barracuda.content.skill.gather;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import org.barracuda.content.action.ActionPromise;
 import org.barracuda.content.action.ActionQueue;
+import org.barracuda.core.game.contract.TextMessage;
 import org.barracuda.core.net.Channel;
 import org.barracuda.horvik.inject.Inject;
 import org.barracuda.model.Entity;
 import org.barracuda.model.actor.Player;
+import org.barracuda.model.fixed.Resource;
 import org.barracuda.model.fixed.ResourceDefinition;
+import org.barracuda.model.fixed.Tool;
 
 public abstract class GathererSkill<T extends Entity> {
 
@@ -30,7 +36,7 @@ public abstract class GathererSkill<T extends Entity> {
 	 * The player
 	 */
 	@Inject private Channel channel;
-
+	
 	/**
 	 * Validates the entity the player is attempting to gather from
 	 * 
@@ -45,16 +51,58 @@ public abstract class GathererSkill<T extends Entity> {
 	 * @param object
 	 * @return
 	 */
-	public void gather(T entity, ResourceDefinition definition) {
+	public ActionPromise gather(T entity, ResourceDefinition definition) {
+		return queue.submit(1, ActionQueue.MAXIMUM_REPETITION, container -> {
+			if (validate(entity) && checkRequirements(definition)) {
+				if (random.nextDouble() <= nextChance(player, definition, null)) {
+					Resource item = randomItem(definition);
+					player.getInventory().add(item.getResource());
+					player.getStats().addExperience(definition.getSkill(), item.getExperience());
+					if (random.nextInt(definition.getCount()) == 0) {
+						// deplete etc
+					}
+				}
+			}
+		}).submit(container -> {
+			// if timer animation etc.
+		}).fail(exception -> channel.write(new TextMessage(exception.getMessage())));
 	}
 
 	/**
+	 * Checks to see if the player is eligible to harvest the resources
 	 * 
 	 * @param definition
 	 * @return
 	 */
 	protected boolean checkRequirements(ResourceDefinition definition) {
-		return true;
+		return false;
+	}
+	
+	/**
+	 * Calculates the frequency at which the player gathers materials
+	 * 
+	 * @param player
+	 * @param definition
+	 * @return
+	 */
+	public double nextChance(Player player, ResourceDefinition definition, Tool tool) {
+		return 0.1;
+	}
+
+	/**
+	 * 
+	 * @param items
+	 * @return
+	 */
+	private Resource randomItem(ResourceDefinition definition) {
+		List<Resource> resources = new ArrayList<>();
+		for (Resource resource : definition.getResources()) {
+			if (resource.getLevel() <= player.getStats().getLevel(definition.getSkill())) {
+				resources.add(resource);
+			}
+		}
+		Resource[] out = resources.toArray(new Resource[0]);
+		return out[random.nextInt(out.length)];
 	}
 
 }
