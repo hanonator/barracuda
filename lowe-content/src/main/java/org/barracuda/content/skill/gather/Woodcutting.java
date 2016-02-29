0e374cd62ec7d95be200a448da3e5798e98fba6c
Hannes Lowette <hanneslowette@live.be>
2016-02-29 12:24:29 +0100

    woodcutting is now a gathering skill

--- lowe-content/src/main/java/org/barracuda/content/skill/gather/Woodcutting.java
+++ lowe-content/src/main/java/org/barracuda/content/skill/gather/Woodcutting.java
@@ -1,5 +1,26 @@
 package org.barracuda.content.skill.gather;
 
-public class Woodcutting {
+import org.barracuda.core.game.event.ObjectInteractionEvent;
+import org.barracuda.horvik.bean.Discoverable;
+import org.barracuda.horvik.context.session.SessionScoped;
+import org.barracuda.horvik.event.Observes;
+import org.barracuda.model.fixed.RSObject;
+
+@Discoverable
+@SessionScoped
+public class Woodcutting extends GathererSkill<RSObject> {
+
+	/**
+	 * 
+	 * @param event
+	 */
+	public void on_object(@Observes ObjectInteractionEvent event) {
+		
+	}
+
+	@Override
+	boolean validate(RSObject entity) {
+		return false;
+	}
 
 }
