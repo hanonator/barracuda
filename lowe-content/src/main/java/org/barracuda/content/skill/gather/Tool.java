3f90f8f4b8165c65d165255a2726c6ae617f9f3a
Hannes Lowette <hanneslowette@live.be>
2016-03-02 13:14:15 +0100

    add equals and hashcode to comply to the checkstyle plugin

--- lowe-content/src/main/java/org/barracuda/content/skill/gather/Tool.java
+++ lowe-content/src/main/java/org/barracuda/content/skill/gather/Tool.java
@@ -36,6 +36,35 @@ public class Tool implements Comparable<Tool> {
 	public int compareTo(Tool tool) {
 		return Integer.compare(level, tool.level);
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + level;
+		result = prime * result + skill;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tool other = (Tool) obj;
+		if (id != other.id)
+			return false;
+		if (level != other.level)
+			return false;
+		if (skill != other.skill)
+			return false;
+		return true;
+	}
+
 
 	/**
 	 * @return the animation
